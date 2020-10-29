14d9d339c2fb0b2bf228d2b681c80d4fa5237661
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2020-10-29 09:13:17 +0300

    Some code added

--- src/com/bsu/Company.java
+++ src/com/bsu/Company.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
+import java.util.List;
 
 class Company {
     private String getNameKey() {
@@ -93,6 +94,10 @@ class Company {
                 + address + ";" + foundationDate.toString() + ";" + employeeNumber + ";"
                 + auditor + ";" + phoneNumber + ";" + eMail + ";" + branch + ";" + activityType + ";"
                 + webPage + ";" + System.lineSeparator());
+    }
+
+    static List<Integer> namesToIndexes(List<CompanyFields>) {
+
     }
 
 }
--- src/com/bsu/Sql_Parser.java
+++ src/com/bsu/Sql_Parser.java
@@ -1,10 +1,7 @@
 package com.bsu;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 public class Sql_Parser {
     public void ParseSqlCommand(String cmd) {
@@ -50,6 +47,17 @@ public class Sql_Parser {
             }
         }
 
-        if (words[3] != "")
+        columnRange++;
+        if (!words[columnRange].equals("company_table")) {
+            throw new InputMismatchException("Error: wrong table name.");
+        }
+
+        columnRange++;
+
+        List<Integer> rowIndexes = new ArrayList<>();
+
+        if (!words[columnRange].equalsIgnoreCase("WHERE")) {
+            rowIndexes = Company.namesToIndexes(fields);
+        }
     }
 }
